2718add453dd0d3f17ffba453b24ab77acef3c32
Kevin Jeyakumar <kevinjk2011@live.com>
2020-09-15 12:13:37 -0400

    Created 2 nodes for hw2, added log INFO and modified launch file to reflect

---
+++ packages/homework2/src/listener.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+
+class Listener:
+    def __init__(self):
+        rospy.Subscriber("delta", Float32, self.callback)
+
+    def callback(self, data):
+        rospy.loginfo(data.data) 
+
+
+if __name__ == '__main__':
+    rospy.init_node('listener')
+    Listener()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
---
+++ packages/homework2/src/talker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+
+class Talker:
+    def __init__(self):
+        self.pub = rospy.Publisher("delta", Float32, queue_size=10)
+
+if __name__ == '__main__':
+    rospy.init_node('talker')
+    Talker()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
