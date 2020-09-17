e667ef433ea9ea7b79458b2cd9fc2bb8a039dda7
Kevin Jeyakumar <kevinjk2011@live.com>
2020-09-16 23:03:37 -0400

    Modified hw2 talker node to publish periodically incrementing values

--- packages/homework2/src/talker.py
+++ packages/homework2/src/talker.py
@@ -6,11 +6,20 @@ from std_msgs.msg import Float32
 class Talker:
     def __init__(self):
         self.pub = rospy.Publisher("/homework1/delta", Float32, queue_size=10)
+        self.x=0
+
+    def talk(self):
+        self.pub.publish(x)
 
 if __name__ == '__main__':
-    rospy.init_node('talker')
-    Talker()
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    try:
+        t=Talker()
+        rospy.init_node('talker')
+        rate=rospy.Rate(0.1) # 0.1Hz
+        while not rospy.is_shutdown():
+            t.talk()
+            t.x+=2
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass 
 
