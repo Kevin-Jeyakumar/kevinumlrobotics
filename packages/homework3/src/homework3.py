80677b6cc9eefc7b1a3c00aa83ed4adcd95da4a1
Kevin Jeyakumar <kevinjk2011@live.com>
2020-09-22 03:21:15 +0000

    Created hw3 launch file and modified topic names

--- packages/homework3/src/homework3.py
+++ packages/homework3/src/homework3.py
@@ -5,13 +5,12 @@ from std_msgs.msg import Float32
 
 class Homework3:
     def __init__(self):
-        rospy.Subscriber("lol_new", Float32, self.callback)
-        self.pub = rospy.Publisher("lol_new1", Float32, queue_size=10)
+        rospy.Subscriber("/homework1/total", Float32, self.callback)
+        self.pub = rospy.Publisher("changed_total", Float32, queue_size=10)
         self.x = 0
 
     def callback(self, data):
-        self.x += data.data
-        self.pub.publish(self.x)
+        self.pub.publish(data.data)
 
 if __name__ == '__main__':
     rospy.init_node('homework3')
