75d1fde47601384a0a7e0a25627ef19f925c93ea
Kevin Jeyakumar <kevinjk2011@live.com>
2020-10-09 04:01:11 +0000

    Created homework4 package

---
+++ packages/homework4/src/homework4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from homework4.msg import homework4_msgs
+
+class Homework4:
+    def __init__(self):
+        rospy.Subscriber("/homework1/total", Float32, self.callback)
+        self.pub = rospy.Publisher("converted_total", homework4_msgs, queue_size=10)
+        self.x = 0 
+        self.unit = "meters"
+
+    def callback(self, data):
+        if rospy.has_param("unit"):
+            self.unit = rospy.get_param("unit")
+        else:
+            self.unit = "meters"
+        if self.unit == "meters" :
+            self.x=data.data*0.3048
+        elif self.unit == "smoots" :
+            self.x = data.data/5.5833
+        elif self.unit == "feet" :
+            self.x = data.data
+
+        temp_string = "Unit: %s; Input: %f feet; Output: %f %s" % (self.unit, data.data, self.x, self.unit)
+        rospy.loginfo(temp_string)
+
+        hw4_msg=homework4_msgs()
+        hw4_msg.num=self.x
+        hw4_msg.units=self.unit_
+
+        self.pub.publish(hw4_msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('homework4_node')
+    hom=Homework4()
+
+
+    rospy.spin()
