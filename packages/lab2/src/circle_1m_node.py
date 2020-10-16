552a72e26c01e7c65d673ddd417d21d6deb99cdc
root <kevinjk2011@live.com>
2020-10-16 03:49:05 +0000

    Created lab2 package

---
+++ packages/lab2/src/circle_1m_node.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from duckietown_msgs.msg import Twist2DStamped
+
+class Homework4:
+    def __init__(self):
+        #rospy.Subscriber("/homework1/total", Float32, self.callback)
+        self.pub = rospy.Publisher("converted_total", duckietown_msgs, queue_size=10)
+
+    def circ(self,y,x)
+        circ_msg=duckietown_msgs()
+        circ_msg.v=y
+        circ_msg.omega=x
+
+        self.pub.publish(circ_msg)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('circle_1m_node')
+        ob=Circle()
+        rate=rospy.Rate(10)
+        while not rospy.is_shutdown():
+            ob.circ(0.4,2)
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
