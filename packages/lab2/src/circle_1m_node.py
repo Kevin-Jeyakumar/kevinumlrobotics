ead91203c5614e372e0de1c6f79af483b0ae6103
Kevin Jeyakumar <kevinjk2011@live.com>
2020-10-18 01:38:37 +0000

    Corrected topic name

--- packages/lab2/src/circle_1m_node.py
+++ packages/lab2/src/circle_1m_node.py
@@ -7,7 +7,7 @@ from duckietown_msgs.msg import Twist2DStamped
 class Circle:
     def __init__(self):
         #rospy.Subscriber("/homework1/total", Float32, self.callback)
-        self.pub = rospy.Publisher("converted_total", duckietown_msgs, queue_size=10)
+        self.pub = rospy.Publisher("/duckduckgone/car_cmd_switch_node/cmd", duckietown_msgs, queue_size=10)
 
     def circ(self,y,x):
         circ_msg=duckietown_msgs()
