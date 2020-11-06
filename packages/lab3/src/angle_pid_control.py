e6aa0ff1df8899c780974d8f759016d7a1ff4a45
Kevin-Jeyakumar <kevinjk2011@live.com>
2020-11-06 03:06:49 +0000

    tweaking speed parameters

--- packages/lab3/src/angle_pid_control.py
+++ packages/lab3/src/angle_pid_control.py
@@ -29,26 +29,27 @@ class Angle_PID(PID):
         temp_time = timing.secs+(timing.nsecs/1000000000)
         rospy.loginfo(("time: {}, d: {}, phi: {}, ".format(temp_time, data.d, data.phi)))
 
-        inp = 0#self.calculateSignal(self,error,temp_time)
+        inp = self.calculateSignal(error,temp_time)
         #if om!=0:
         #    self.vel = 0
         #    self.move(self, 0, (self.om*10))
         #else:
         #    self.move(self, self.vel, 0)
         if inp>0:
-            self.om=6
+            om = -self.om
         elif inp<0:
-            self.om=-6
+            om = self.om
         else:
-            self.om=0
+            om=0
+        vel = self.vel
 
-        #self.move(self)
+        self.move(vel,om)
 
 
-    def move(self):
+    def move(self,vel,om):
         move_msg = Twist2DStamped()
-        move_msg.v = self.vel
-        move_msg.omega = self.om
+        move_msg.v = vel
+        move_msg.omega = om
 
         self.pub.publish(move_msg)
 
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     try:
         rospy.init_node('angle_pid_controller_node')
         timing = rospy.get_rostime()
-        Angle_PID(0.185, 0, 0, (timing.secs+(timing.nsecs)), 0.4, 6)
+        Angle_PID(0.185, 0, 0, (timing.secs+(timing.nsecs)), 0.2, 4)
         #hw6 values: p=0.185, i=0.00009, d=1.9
 
         rospy.spin()
