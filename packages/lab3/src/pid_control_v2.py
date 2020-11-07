0568e3be0906ebf1360fe930988b5bd49034e5f1
Kevin Jeyakumar <kevinjk2011@live.com>
2020-11-07 20:12:47 +0000

    designed lab3 new approach and new node

--- packages/lab3/src/pid_control_v2.py
+++ packages/lab3/src/pid_control_v2.py
@@ -5,46 +5,62 @@ from std_msgs.msg import Float32
 from duckietown_msgs.msg import *
 from PID_class import PID
 
-class Angle_PID(PID):
-    def __init__(self,p,i,d,time,vel,om):
-        PID.__init__(self,p,i,d,time)
+class PID_Controller_v2():
+    def __init__(self,time):
+        #hw6 values: p=0.185, i=0.00009, d=1.9
+        self.d_obj = PID(2,0,0,time)
+        self.phi_obj = PID(2,0,0,time)
         rospy.Subscriber("lane_filter_node/lane_pose", LanePose, self.callback)
         self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
-        self.pub1 = rospy.Publisher("my_angle_error", Float32, queue_size=10)
-        self.pub2 = rospy.Publisher("my_angle_desired", Float32, queue_size=10)
-        self.vel = vel
-        self.om = om
-        rospy.set_param("p_phi",self.kp)
-        rospy.set_param("i_phi",self.ki)
-        rospy.set_param("d_phi",self.kd)
+        self.pub1 = rospy.Publisher("my_dist_error", Float32, queue_size=10)
+        self.pub2 = rospy.Publisher("my_phi_error", Float32, queue_size=10)
+        self.pub3 = rospy.Publisher("my_desired", Float32, queue_size=10)
+        self.vel = 0.1
+        self.om = 0.6
+       
+        rospy.set_param("p_d",self.d_obj.kp)
+        rospy.set_param("i_d",self.d_obj.ki)
+        rospy.set_param("d_d",self.d_obj.kd)
 
+        rospy.set_param("p_phi",self.phi_obj.kp)
+        rospy.set_param("i_phi",self.phi_obj.ki)
+        rospy.set_param("d_phi",self.phi_obj.kd)
+       
     def callback(self,data):
-        tmp_p = rospy.get_param("p_phi")
-        tmp_i = rospy.get_param("i_phi")
-        tmp_d = rospy.get_param("d_phi")
-        if tmp_p!=self.kp or tmp_i!=self.ki or tmp_d!=self.kd or self.integral>1000000000:
-            self.integral = 0
-            self.changePID(tmp_p, tmp_i, tmp_d)
+        tmp_d_p = rospy.get_param("p_d")
+        tmp_d_i = rospy.get_param("i_d")
+        tmp_d_d = rospy.get_param("d_d")
+        tmp_phi_d = rospy.get_param("p_phi")
+        tmp_phi_d = rospy.get_param("i_phi")
+        tmp_phi_d = rospy.get_param("d_phi")
+        self.d_obj.changePID(tmp_d_p, tmp_d_i, tmp_d_d)
+        self.phi_obj.changePID(tmp_phi_p, tmp_phi_i, tmp_phi_d)
+         if self.d_obj.integral>1000000000:
+            self.d_obj.integral = 0
+        if self.phi_obj.integral>1000000000:
+            self.phi_obj.integral = 0
 
-        error = 0 - data.phi
+        d_error = 0 - data.d
+        phi_error = 0 - data.phi
         timing = rospy.get_rostime()
         temp_time = timing.secs+(timing.nsecs/1000000000)
 
         temp_time_diff = temp_time - self.past_time_stamp
-        inp = self.calculateSignal(error,temp_time)
-        rospy.loginfo(("time: {:2.3f}, d: {:2.3f}, phi: {:2.3f}, input: {:2.3f} - Angle".format(temp_time_diff, data.d, data.phi, inp)))
-        self.pub1.publish(error)
-        self.pub1.publish(0)
-        if inp<0.2 and inp>-0.2:
-            inp=0
+        d_inp = self.d_obj.calculateSignal(d_error,temp_time)
+        phi_inp = self.phi_obj.calculateSignal(phi_error,temp_time)
+        rospy.loginfo(("time: {:2.3f}, d: {:2.3f}, phi: {:2.3f}, d_input: {:2.3f}, phi_input: {:2.3f}".format(temp_time_diff, data.d, data.phi, d_inp, phi_inp)))
+        self.pub1.publish(d_error)
+        self.pub2.publish(phi_error)
+        self.pub3.publish(0)
 
         vel = self.vel
-        rospy.set_param("mypub_phi","False")
-        if inp!=0:
-            rospy.set_param("mypub_phi","True")
-        #if rospy.get_param("mypub_d") == "True":
-        #    return
-        om = inp
+        om = d_inp + phi_inp
+        if om<0.2 and om>-0.2:
+            om = 0
+        if om>2:
+            om = 2
+        elif om<-2:
+            om = -2 
 
         self.move(vel,om)
 
@@ -59,20 +75,13 @@ class Angle_PID(PID):
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('angle_pid_controller_node')
+        rospy.init_node('pid_controller_node_v2')
         timing = rospy.get_rostime()
-        Angle_PID(1, 0, 0, (timing.secs+(timing.nsecs/1000000000)), 0.1, 0.6)
-        #hw6 values: p=0.185, i=0.00009, d=1.9
+        PID_Controller_v2((timing.secs+(timing.nsecs/1000000000)))
 
         rospy.spin()
-        #rate=rospy.Rate(10)
 
-        #for count in range(0,100):#10 second timer
-        #    ob.move(0.7,1.5)
-        #    rate.sleep()
-        #ob.move(0,0)
     except rospy.ROSInterruptException:
         pass
-
 
 
