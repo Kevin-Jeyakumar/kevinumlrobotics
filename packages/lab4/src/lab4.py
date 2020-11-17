b9d5b7afa55ff35f5faa7ed5be47dc8a9fb68897
Kevin Jeyakumar <kevinjk2011@live.com>
2020-11-17 04:10:44 +0000

    imported wheel velocity message in src and added group name as vehicle name in launch for lab4

--- packages/lab4/src/lab4.py
+++ packages/lab4/src/lab4.py
@@ -2,20 +2,20 @@
 
 import rospy
 import numpy as np
-from duckiebot_msgs.msg import *
+from duckietown_msgs.msg import WheelsCmdStamped
 
 class Lab4:
     def __init__(self):
-        rospy.Subscriber("wheels_driver_node/wheeld_cmd", DistWheel, self.callback)
+        rospy.Subscriber("wheels_driver_node/wheeld_cmd", WheelsCmdStamped, self.callback)
         #self.pub = rospy.Publisher("/pose", Pose2D, queue_size=10)
         #self.pos = Pose2D()
         self.x = 0
         self.y = 0
         self.theta = 0
-        self.x_list = list()
-        self.y_list = list()
-        self.x_list.append(self.x)
-        self.y_list.append(self.y)
+        #self.x_list = list()
+        #self.y_list = list()
+        #self.x_list.append(self.x)
+        #self.y_list.append(self.y)
         my_time = rospy.get_rostime()
         self.past_time = my_time.secs + (my_time.nsecs/1000000000)
 
@@ -23,17 +23,22 @@ class Lab4:
         my_time = rospy.get_rostime()
         del_t = my_time.secs + (my_time.nsecs/1000000000)
         del_t = self.past_time - del_t
-        del_s = data.dist_wheel_left + data.dist_wheel_right
+        dist_left = data.vel_left * del_t
+        dist_right = data.vel_right * del_t
+        del_s = dist_left + dist_right
         del_s /= 2
-        del_theta = data.dist_wheel_right - data.dist_wheel_left
+        del_theta = dist_right - dist_left
         del_theta /= 0.1
 
         self.x += del_s * np.cos(self.pos.theta + (del_theta/2))
         self.y += del_s * np.sin(self.pos.theta + (del_theta/2))
         self.theta += del_theta
 
-        self.x_list.append(self.x)
-        self.y_list.append(self.y)
+        #self.x_list.append(self.x)
+        #self.y_list.append(self.y)
+
+        mylogstr = "x:",self.x,"y:",self.y
+        rospy.loginfo(mylogstr) #"x: {}, y: {}".format(self.x,self.y))
 
 if __name__ == "__main__":
     rospy.init_node("lab4_node")
