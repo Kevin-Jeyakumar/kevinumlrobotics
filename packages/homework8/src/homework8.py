65bb0de17d7588b179d9d210a8a08d708aaab13a
Kevin Jeyakumar <kevinjk2011@live.com>
2020-11-30 18:14:59 -0500

    added extra output topics for hw8

--- packages/homework8/src/homework8.py
+++ packages/homework8/src/homework8.py
@@ -12,6 +12,8 @@ class Homework8:
         self.pub = rospy.Publisher("image_cropped", Image, queue_size=10)
         self.pub1 = rospy.Publisher("image_white", Image, queue_size=10)
         self.pub2 = rospy.Publisher("image_yellow", Image, queue_size=10)
+        self.pub3 = rospy.Publisher("image_white_filtered", Image, queue_size=10)
+        self.pub4 = rospy.Publisher("image_yellow_filtered", Image, queue_size=10)
         self.bridge = CvBridge()
 
     def callback(self, data):
@@ -29,17 +31,21 @@ class Homework8:
         cv_eroded_white_filter = cv2.erode(cv_white_filter, kernel)
         cv_white_mask = cv2.dilate(cv_eroded_white_filter, kernel)
         cv_white_mask = cv2.dilate(cv_white_mask, kernel)
+        ros_white = self.bridge.cv2_to_imgmsg(cv_white_mask, "mono8")
+        self.pub1.publish(ros_white)
         cv_white_filtered_image = cv2.bitwise_and(cv_cropped, cv_cropped, mask=cv_white_mask)
         ros_white_filtered = self.bridge.cv2_to_imgmsg(cv_white_filtered_image, "bgr8")
-        self.pub1.publish(ros_white_filtered)
+        self.pub3.publish(ros_white_filtered)
 
         cv_yellow_filter = cv2.inRange(cv_hsv, (25,100,200), (40, 255, 255))
         cv_eroded_yellow_filter = cv2.erode(cv_yellow_filter, kernel)
         cv_yellow_mask = cv2.dilate(cv_eroded_yellow_filter, kernel)
         cv_yellow_mask = cv2.dilate(cv_yellow_mask, kernel)
+        ros_yellow = self.bridge.cv2_to_imgmsg(cv_yellow_mask, "mono8")
+        self.pub2.publish(ros_yellow)
         cv_yellow_filtered_image = cv2.bitwise_and(cv_cropped, cv_cropped, mask=cv_yellow_mask)
         ros_yellow_filtered = self.bridge.cv2_to_imgmsg(cv_yellow_filtered_image, "bgr8")
-        self.pub2.publish(ros_yellow_filtered)
+        self.pub4.publish(ros_yellow_filtered)
 
 if __name__ == "__main__":
     rospy.init_node("homework8_node")
